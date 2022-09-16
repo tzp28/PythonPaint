f083ae8f3a7c969df3e03a5ece34864e5cb7fe63
tzp28 <tpeters2699@gmail.com>
2022-09-16 00:02:54 -0400

    Paint Git Push v2

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
-print("Paint Git Push v1")
+class Home:
+    def __init__(self):
+        self.rooms = []
+
+    def addRoom(self, name):
+        self.rooms.append(name)
+
+
+class Room:
+    def __init__(self, walls, name):
+        self.walls = walls
+        self.name = name
+
+
+class Wall:
+    def __init__(self, height, width, windows, doors):
+        self.height = height
+        self.width = width
+        self.height = windows
+        self.width = doors
+
+
+if __name__ == '__main__':
+    roomNum = input("Enter the amount of rooms in your home: ")
+    userHouse = Home()
+    for room in range(int(roomNum)):
+        roomName = input("Enter the name of room {}: ".format(room+1))
+        userHouse.addRoom(roomName)
+        roomName = input("Enter the # of walls in " + roomName + ": ")
+
+    print(userHouse.rooms)
+
+
