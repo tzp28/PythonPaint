287984d0a151620ca905fdf5cca439e13752dff2
tzp28 <tpeters2699@gmail.com>
2022-09-19 18:43:29 -0400

    Paint Git Push v3

--- main.py
+++ main.py
@@ -7,27 +7,86 @@ class Home:
 
 
 class Room:
-    def __init__(self, walls, name):
-        self.walls = walls
-        self.name = name
+    def __init__(self):
+        self.walls = []
+
+    def addWall(self, name):
+        self.walls.append(name)
 
 
 class Wall:
-    def __init__(self, height, width, windows, doors):
+    def __init__(self, height, width, windows, doors, area):
         self.height = height
         self.width = width
-        self.height = windows
-        self.width = doors
+        self.windows = windows
+        self.doors = doors
+        self.area = area
 
 
 if __name__ == '__main__':
+    units = input("Which units do you use for length? (Enter Imperial or Metric, please): ")
+    if units == "Imperial":
+        units = "sq ft"
+    if units == "Metric":
+        units = "sq meters"
     roomNum = input("Enter the amount of rooms in your home: ")
     userHouse = Home()
+    userRoom = Room()
+    roomSurArea = 0
+    totSurArea = 0
+    totWinArea = 0
+    totDoorArea = 0
     for room in range(int(roomNum)):
-        roomName = input("Enter the name of room {}: ".format(room+1))
+        roomName = input("Enter the name of room {}: ".format(room + 1))
         userHouse.addRoom(roomName)
-        roomName = input("Enter the # of walls in " + roomName + ": ")
+        wallNum = input("Enter the # of walls in " + roomName + ": ")
+        for wall in range(int(wallNum)):
+            wallHeight = input("Enter the height of the wall " + str(wall + 1) + ": ")
+            wallWidth = input("Enter the width of the wall " + str(wall + 1) + ": ")
+            wallWindows = input("Enter the # of windows on wall " + str(wall + 1) + ": ")
+            if int(wallWindows) > 0:
+                for window in range(int(wallWindows)):
+                    windowHeight = input("Enter the height of the window " + str(window + 1) + ": ")
+                    windowWidth = input("Enter the width of the window " + str(window + 1) + ": ")
+                    windowArea = int(windowHeight) * int(windowWidth)
+                    totWinArea += windowArea
+            else:
+                totWinArea = 0
+            wallDoors = input("Enter the # of doors on wall " + str(wall + 1) + ": ")
+            if int(wallDoors) > 0:
+                for door in range(int(wallDoors)):
+                    doorHeight = input("Enter the height of the door " + str(door + 1) + ": ")
+                    doorWidth = input("Enter the width of the door " + str(door + 1) + ": ")
+                    doorArea = int(doorHeight) * int(doorWidth)
+                    totDoorArea += doorArea
+            else:
+                totDoorArea = 0
+            wallArea = ((int(wallHeight) * int(wallWidth)) - totWinArea - totDoorArea)
+            userWall = Wall(wallHeight, wallWidth, wallWindows, wallDoors, wallArea)
+            userRoom.addWall(userWall)
+            print(userWall.height, userWall.width, userWall.windows, userWall.doors, userWall.area)
+            roomSurArea += userWall.area
+            print("The Surface Area for wall " + str(wall + 1) + " is: " + str(userWall.area) + " " + units)
+        paintCeiling = input("Do you want to paint the ceiling (Enter Yes or No, please): ")
+        if paintCeiling == "Yes":
+            ceilingHeight = int(input("Enter the height of the ceiling"))
+            ceilingWidth = int(input("Enter the width of the ceiling"))
+            ceilingArea = ceilingHeight * ceilingWidth
+        if paintCeiling == "No":
+            ceilingHeight = 0
+            ceilingWidth = 0
+            ceilingArea = 0
+        roomSurArea += ceilingArea
+        print("The Surface Area for " + roomName + " is: " + str(roomSurArea) + " " + units)
+        coats = int(input("How many coats of paint do you want for " + roomName + ": "))
+        if units == "sq ft":
+            print((roomSurArea * coats) / 400)
+            gallons = round((roomSurArea * coats) / 400, 0)
+            print("You need at least " + str(gallons) + " gallons to paint all of " + roomName)
+        if units == "sq meters":
+            units = "sq meters"
 
-    print(userHouse.rooms)
-
-
+        # print("Select the Paint Brand" + roomName + ": ")
+        # print("Select the brand of " + roomName + ": ")
+    # print(userHouse.rooms)
+    # print(userRoom.walls)
